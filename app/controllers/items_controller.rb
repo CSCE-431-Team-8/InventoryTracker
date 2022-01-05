b9da9e82fbe728bb90f731545c3b5cb1207fd4cd
Ryan Parker <ryan@ryanparker.org>
2022-01-05 10:32:08 +0200

    Added basic item views

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,8 @@
+class ItemsController < ApplicationController
+    def items
+        @items = ["The Martian", "Project Hail Mary", "Percy Jackso and the Lightning Theif"]
+    end
+
+    def addItem
+        
+end

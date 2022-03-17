40ec69fcad0b53f240e4f2e3bc2ed776c086d03c
Ryan Parker <ryan@ryanparker.org>
2022-03-16 23:26:13 -0500

    Fixed small test errors

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
 
     before(:all) do
-        @item = Item.create(name: "Basketball", quantity_remaining: 10, quantity_total: 15, price: 10)
+        @item = Item.create(name: "Basketballs spec", quantity_remaining: 10, quantity_total: 15, price: 10)
     end
 
     it 'checks that an item can be created' do
@@ -11,17 +11,17 @@ RSpec.describe Item, type: :model do
     end
 
     it 'checks that an item can be read' do
-        expect(Item.find_by_name("Basketball")).to eq(@item)
+        expect(Item.find_by_name("Basketballs spec")).to eq(@item)
     end
 
     it 'checks that an item can be updated' do
-        @item.update(:name => "Tennisball")
-        expect(Item.find_by_name("Tennisball")).to eq(@item)
+        @item.update(:name => "Tennisball spec")
+        expect(Item.find_by_name("Tennisball spec")).to eq(@item)
     end
 
     it 'checks that an item can be destroyed' do
         @item.destroy
-        expect(Item.find_by_name("Tennisball")).to be_nil
+        expect(Item.find_by_name("Tennisball spec")).to be_nil
     end
 
     it 'is not valid without a name' do 
@@ -30,12 +30,12 @@ RSpec.describe Item, type: :model do
     end
 
     it 'is not valid without a quantity remaining' do 
-        @item_temp2 = Item.create(name: "Baseball", quantity_remaining: nil, quantity_total: 15, price: 10)
+        @item_temp2 = Item.create(name: "Baseball spec", quantity_remaining: nil, quantity_total: 15, price: 10)
         expect(@item_temp2).not_to be_valid
     end
 
     it 'is not valid if total quantity is less than 0' do 
-        @item_temp3 = Item.create(name: "Baseball", quantity_remaining: 10, quantity_total: -1, price: 10)
+        @item_temp3 = Item.create(name: "Baseball spec", quantity_remaining: 10, quantity_total: -1, price: 10)
         expect(@item_temp3).not_to be_valid
     end
 

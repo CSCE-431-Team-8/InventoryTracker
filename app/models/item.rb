be212960a39f683dd16df47b68e04ad0ac80014f
Ryan Parker <ryan@ryanparker.org>
2022-03-21 04:14:23 -0500

    Added validation and proper updating for the rentable attribute

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ActiveRecord::Base
     validates :name, presence: true
     validates :quantity_total, numericality: { greater_than_or_equal_to: 0}
     validates :quantity_remaining, presence: true
-    validates :rentable, presence: true
+    validates :rentable, inclusion: { in: [ true, false ] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 
 more_items = [
     {:name => 'Golf Ball', :description => 'A ball for golf', :quantity_remaining => 20, 
-        :quantity_total => 25, :rentable => true, :price => 5, :organization => "TAMU", :location => "Gear Locker"},
+        :quantity_total => 25, :rentable => false, :price => 5, :organization => "TAMU", :location => "Gear Locker"},
     {:name => 'Tennis Ball', :description => 'A ball for tennis', :quantity_remaining => 10, 
         :quantity_total => 20, :rentable => true, :price => 10, :organization => "TAMU", :location => "Gear Locker"},
     {:name => 'Basket Ball', :description => 'A ball for basketball', :quantity_remaining => 5, 
